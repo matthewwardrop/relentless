657b00e190a3a66bdc71f6d821c64cf79788570a
Matthew Wardrop <mister.wardrop@gmail.com>
2015-06-19 11:45:26 +1000

    Bump version number to 0.2 .

--- relentless/__init__.py
+++ relentless/__init__.py
@@ -1,2 +1,5 @@
+__author__ = "Matthew Wardrop <mister.wardrop@gmail.com>"
+__version__ = 0.2
+
 from computations import *
 from testers import *
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='relentless',
-      version='0.1',
+      version='0.2',
       description='A library to assist in testing of programming competition solutions.',
       author='Matthew Wardrop',
       author_email='mister.wardrop@gmail.com',
