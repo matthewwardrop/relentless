29b2cd4ec9cd48a693dd40ae8c3b2ffc9bbcbf73
Matthew Wardrop <mister.wardrop@gmail.com>
2015-06-24 12:05:27 +1000

    Improved diff_array plotting.

--- relentless/utils/diff_array.py
+++ relentless/utils/diff_array.py
@@ -10,7 +10,7 @@ class DiffArray(XeLaTeX):
 		with open(os.path.join(os.path.dirname(__file__),'templates','diff_array.tex')) as f:
 			return f.read()
 
-	def process(self, output='diff', new=None, old=None, tasks=None, columns=10, maximise=True):
+	def process(self, output='diff', new=None, old=None, tasks=None, columns=None, maximise=True):
 
 		if new is None or old is None:
 			raise ValueError("new and old must both be numpy arrays of the same dimension")
@@ -26,34 +26,32 @@ class DiffArray(XeLaTeX):
 		while not it.finished:
 			diffs.append( (it.index, float(it[0]) ) )
 			it.iternext()
-			
+
+		if columns is None:
+			columns = int(np.ceil(np.sqrt(len(diffs))))
+
 		total_new = np.sum(new)
 		total_old = np.sum(old)
-		
+
+		if total_old == 0:
+			total_old = 1
+
 		wins = np.sum(r>0)
 		losses = np.sum(r<0)
 		LOS = 0.5*(1+math.erf( float(wins - losses)/math.sqrt(wins+losses)/math.sqrt(2) ))
-		
-		# TODO: Simplify with numpy
-		max_diff = 1.0
-		max_improvement = 0
-		max_regression = 0
-		avg_improvement = 0
+
+		max_diff = np.max(np.abs(r))
+		if maximise:
+			max_improvement = np.max(r)
+			max_regression = np.min(r)
+			avg_improvement = np.average(r)
+		else:
+			max_improvement = np.max(-r)
+			max_regression = np.min(-r)
+			avg_improvement = np.average(-r)
 
 		if tasks is None:
 			tasks = range(1, len(diffs)+1)
-
-		for (index, rel) in diffs:
-			rel = float(rel)
-			if maximise:
-				max_improvement = rel if max_improvement < rel else max_improvement
-				max_regression = rel if max_regression > rel else max_regression
-			else:
-				max_improvement = rel if max_improvement > rel else max_improvement
-				max_regression = rel if max_regression < rel else max_regression
-			max_diff = abs(rel) if abs(rel) > max_diff else max_diff
-			avg_improvement += rel
-		avg_improvement /= len(diffs)
 
 		output['summary'] = r"""	\node[rectangle, align=center] at (%.1f, 1) {Best: %.3f | Worst: %.3f | Avg: %.3f | Rel: %.3f \\ Wins: %d | Losses: %d | LOS: %.3f};""" % (float(columns-1)/2, max_improvement, max_regression, avg_improvement, float(total_new)/float(total_old), wins, losses, LOS)
 
